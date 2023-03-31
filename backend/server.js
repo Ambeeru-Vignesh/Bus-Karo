a36d64d90ad8e707b6811a338b3ea5124960eac2
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-03-31 23:41:31 +0530

    Project Completed.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./config/db");
 app.use(express.json());
-
+const path = require("path");
 const userRoutes = require("./routes/userRoutes");
 const busRoutes = require("./routes/busRoutes");
 const bookingRoutes = require("./routes/bookingRoute");
@@ -12,6 +12,20 @@ connectDB();
 app.use("/api/users", userRoutes);
 app.use("/api/buses", busRoutes);
 app.use("/api/bookings", bookingRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running....");
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
